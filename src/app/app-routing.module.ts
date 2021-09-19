9ca44185bc388a1fa33888c0298ed544a0f213a1
pamelallasaca <cpamela505@gmail.com>
2021-09-19 17:01:13 -0500

    frontend pamela

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AddMedicamentoComponent } from './components/add-medicamento/add-medicamento.component';
+
+
+const routes: Routes = [
+{path:"addMedicamento", component:AddMedicamentoComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+
+export class AppRoutingModule { }
---
+++ src/app/components/add-medicamento/add-medicamento.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Medicamento } from 'src/app/models/medicamento.model';
+import { MedicamentoService } from 'src/app/services/medicamento.service';
+
+@Component({
+  selector: 'app-add-medicamento',
+  templateUrl: './add-medicamento.component.html',
+  styleUrls: ['./add-medicamento.component.css']
+})
+export class AddMedicamentoComponent implements OnInit {
+ 
+
+  constructor(private medicamentoService:MedicamentoService) { }
+
+
+medicamento: Medicamento = { }
+
+
+saveMedicamento(){
+console.log(">> saveMedicamento");
+console.log(this.medicamento);
+
+  this.medicamentoService.create(this.medicamento).subscribe(
+    response => {
+      console.log(response.mensaje);
+      alert(response.mensaje)
+    },
+    error => {
+      console.log(error);
+    },
+  );
+   
+   }
+   
+
+  
+
+
+  
+  
+
+  ngOnInit(): void {
+  }
+
+}
